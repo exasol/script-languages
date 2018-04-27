0c34ce4e338bd0863cd8d89d0a808cabcdbfbc00
stmandl <stefan.mandl@exasol.com>
2018-04-27 17:01:41 +0200

    added libraries

--- linux_container/make_container.sh
+++ linux_container/make_container.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+VERSION_NAME="ScriptLanguages-"$(date --iso-860)
 echo Building image
 docker build --rm=true --tag exasol/script_lang_base_container .
 echo Creating image
-docker run --name exasol_script_lang_base_container exasol/script_lang_base_container 
+docker run --dns=172.17.0.1 --name exasol_script_lang_base_container exasol/script_lang_base_container 
 docker stop exasol_script_lang_base_container
 echo Exporting container
-docker export exasol_script_lang_base_container > ScriptLanguages-6.0.2.tar
+docker export exasol_script_lang_base_container > $VERSION_NAME.tar
 echo Cleanup Docker
 docker rm exasol_script_lang_base_container
 docker rmi exasol/script_lang_base_container
 echo Preparing Container:
 echo 1. Unpack
-mkdir ScriptLanguages-6.0.2
-tar xf ScriptLanguages-6.0.2.tar -C ScriptLanguages-6.0.2 --exclude=dev --exclude=proc
+mkdir $VERSION_NAME
+tar xf $VERSION_NAME.tar -C $VERSION_NAME --exclude=dev --exclude=proc
 echo 2. Creating Dirs
-mkdir ScriptLanguages-6.0.2/conf ScriptLanguages-6.0.2/proc ScriptLanguages-6.0.2/dev ScriptLanguages-6.0.2/exasol ScriptLanguages-6.0.2/buckets
+mkdir $VERSION_NAME/conf $VERSION_NAME/proc $VERSION_NAME/dev $VERSION_NAME/exasol $VERSION_NAME/buckets
 echo 3. Creating links
-rm ScriptLanguages-6.0.2/etc/resolv.conf ScriptLanguages-6.0.2/etc/hosts
-ln -s /conf/resolv.conf ScriptLanguages-6.0.2/etc/resolv.conf
-ln -s /conf/hosts ScriptLanguages-6.0.2/etc/hosts
+rm $VERSION_NAME/etc/resolv.conf $VERSION_NAME/etc/hosts
+ln -s /conf/resolv.conf $VERSION_NAME/etc/resolv.conf
+ln -s /conf/hosts $VERSION_NAME/etc/hosts
 echo 4. Package
-cd ScriptLanguages-6.0.2
-tar --numeric-owner --owner=0 --group=0 -zcf ../ScriptLanguages-6.0.2.tar.gz *
+cd $VERSION_NAME
+tar --numeric-owner --owner=0 --group=0 -zcf ../$VERSION_NAME.tar.gz *
 cd ..
 echo 5. Cleanup temp files
-rm ScriptLanguages-6.0.2.tar
-rm -rf ScriptLanguages-6.0.2
+rm $VERSION_NAME.tar
+rm -rf $VERSION_NAME
 echo Done.
