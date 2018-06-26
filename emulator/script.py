eb81775b24f29eca4804a80f8c0409276f3b2707
stmandl <stefan.mandl@exasol.com>
2018-06-26 12:30:50 +0200

    improved conda support

--- emulator/script.py
+++ emulator/script.py
@@ -5,7 +5,7 @@
 #output_column: a,int64,INT,None,None,None
 #output_column: b,string,VARCHAR(100),100,None,None
 #output_type: EMITS
-import tensorflow as tf
+#import tensorflow as tf
 
 def run(ctx):
     while True:
