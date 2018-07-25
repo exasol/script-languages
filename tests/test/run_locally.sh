2e8fd7aac113ec44b4174d6d5f562b9b5d86c404
Hendrik Cech <hendrik.cech@gmail.com>
2018-07-25 16:41:26 +0200

    Rework interface of run_locally.sh and integrate with test script

--- tests/test/run_locally.sh
+++ tests/test/run_locally.sh
@@ -1,55 +1,102 @@
 #!/bin/bash
-set -ux
+set -u
+
+die() { echo "ERROR:" "$@" >&2; exit 1; }
 
 # $1: path to python test file
-# $2...: additional arguments
+# $2: exasol server address
+# $3...: additional arguments passed to tests
 function run_test() {
-    EXAPLUS=/opt/EXAplus-6.0.10/exaplus \
-	   python -tt "$1" \
-            --driver=`pwd`/../../lib/EXASOL_ODBC-6.0.8/lib/linux/x86_64/libexaodbc-uo2214lv2.so \
-            --server=192.168.122.75:8563 \
-            --jdbc-path `pwd`/../../lib/EXASOL_JDBC-6.0.8/exajdbc.jar
+    # echo "execute $@"
+    export EXAPLUS=/opt/EXAplus-6.0.10/exaplus
+    cmd=$(echo python -tt "$1" \
+                      --driver=$(pwd)/../../downloads/EXASOL_ODBC-6.0.8/lib/linux/x86_64/libexaodbc-uo2214lv2.so \
+                      --server "$2" \
+                      --jdbc-path $(pwd)/../../EXASOL_JDBC-6.0.8/exajdbc.jar \
+                      "${@:3}" # for, e.g., --lang
+         )
+    echo "$cmd"
+    $cmd
 }
-export -f run_test
 
 # $1: run generic tests for lang $1, e.g., "java"
+# $2: exasol server address
+# $3: language definition
 function run_generic_tests() {
+    echo "Run generic language test for $1"
     for test in generic/*.py; do
-        run_test "$test" --lang $1
+        run_test "$test" "$2" --script-languages "$3" --lang "$1"
     done
 }
-function run_generic_tests_parallel() {
-    find generic -iname '*py' | parallel "run_test {} --lang $1"
-}
-export -f run_generic_tests_parallel
-
 
 # $1: run specific and generic tests for $1, e.g., "java"
-run_tests_for_lang() {
-    lang="$1"
-    echo "--- START TEST ${lang} ---"
-    for test in $lang/*.py; do
-        run_test "$test"
+# $2: exasol server address
+# $3: language definition
+function run_tests_in_folder() {
+    folder="$1"
+    echo "--- START TEST ${folder} ---"
+    for test in $folder/*.py; do
+        run_test "$test" "$2" --script-languages "$3"
     done
-    run_generic_tests "$lang"
-    echo "--- END TEST ${lang} ---"
-}
-run_tests_for_lang_parallel() {
-    lang="$1"
-    echo "--- START PARALLEL TEST ${lang} ---"
-    find $lang -iname '*py' | parallel 'run_test {}'
-    run_generic_tests_parallel "$lang"
-    echo "--- END PARALLEL TEST ${lang} ---"
+    echo "--- END TEST ${folder} ---"
 }
 
 
-if [ ! -z "${1-}" ]; then
-    run_tests_for_lang "$1" 2>&1 | tee "run_locally-$1.out"
-    exit $?
+# ./run-locally.sh --server 192... --test-config /home/...mini/../testconfig
+while true; do
+    case "${1-}" in
+        --server) server="$2"; shift 2;;
+        --test-config) test_config="$2"; shift 2;;
+        --) shift; break;;
+        *) echo "Usage: $0"
+		       echo "Options:"
+		       echo "  [--server=<host:port>]                Address of Exasol database instance"
+		       echo "  [--test-config=<path>]                Path to flavor test config file"
+		       echo "  [-h|--help]                           Print this help."; exit 0;;
+    esac
+done
+
+if [ -z "$server" ]; then die "--server is required"; fi
+
+if [ ! -f "$test_config" ]; then
+    echo "testconfig for flavor $FLAVOR does not exist here: $config_file"
+    exit 1
 fi
 
-# ext-python
-languages=(java lua python r)
-for lang in ${languages[@]}; do
-    run_tests_for_lang "$lang" 2>&1 | tee "run_locally-$lang.out"
-done
+typeset -A config
+config=( )
+
+while read line
+do
+    if echo $line | grep -F = &>/dev/null
+    then
+        varname=$(echo "$line" | cut -d '=' -f 1)
+	      if [[ $varname == "#*" ]]; then continue; fi
+        config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+    fi
+done < $test_config
+
+for x in "${!config[@]}"; do printf "[%s]=%s\n" "$x" "${config[$x]}" ; done
+
+if [ ! -z "${config[generic_language_tests]-}" ]; then
+    for folder in ${config[generic_language_tests]}; do
+        echo "$folder"
+        run_generic_tests "$folder" "$server" "${config[language_definition]}"
+    done
+fi
+
+if [ ! -z "${config[test_folders]-}" ]; then
+    for folder in ${config[test_folders]}; do
+        run_tests_in_folder "$folder" "$server" "${config[language_definition]}"
+    done
+fi
+
+# if [ ! -z "${1-}" ]; then
+#     run_tests_in_folder "$1" 2>&1 | tee "run_locally-$1.out"
+#     exit $?
+# else
+#     languages=(java lua python r)
+#     for lang in ${languages[@]}; do
+#         run_tests_in_folder "$lang" 2>&1 | tee "run_locally-$lang.out"
+#     done
+# fi
