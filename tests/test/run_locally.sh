ecfa2b2d24e08683fde9d8b6144429b59b26388d
Hendrik Cech <hendrik.cech@gmail.com>
2018-08-06 13:09:27 +0200

    Fix file paths

--- tests/test/run_locally.sh
+++ tests/test/run_locally.sh
@@ -10,9 +10,9 @@ die() { echo "ERROR:" "$@" >&2; exit 1; }
 function run_test() {
     # echo "execute $@"
     cmd=$(echo python2 -tt "$1" \
-                       --driver=$(pwd)/../../downloads/EXASOL_ODBC-6.0.8/lib/linux/x86_64/libexaodbc-uo2214lv2.so \
+                       --driver=$(pwd)/../../downloads/ODBC/lib/linux/x86_64/libexaodbc-uo2214lv2.so \
                        --server "$2" \
-                       --jdbc-path $(pwd)/../../EXASOL_JDBC-6.0.8/exajdbc.jar \
+                       --jdbc-path $(pwd)/../../downloads/JDBC/exajdbc.jar \
                        "${@:3}" # for, e.g., --lang
          )
     echo "$cmd"
