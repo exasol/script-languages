779484fb939609323a596e47083b69ec40b99d2d
root <root@ip-10-0-0-52.eu-west-1.compute.internal>
2018-11-05 15:18:46 +0000

    Python dataframe support

--- src/build_client.sh
+++ src/build_client.sh
@@ -220,9 +220,7 @@ if [ "$ENABLE_PYTHON3_IMPL" = "yes" ]; then
     echo "Compiling Python3 specific code with these CXXFLAGS:$CXXFLAGS"
     g++ -o exascript_python.o -c exascript_python.cc $CXXFLAGS || die "Failed to compile exascript_python.o"
     g++ -o pythoncontainer.o -c pythoncontainer.cc $CXXFLAGS || die "Failed to compile pythoncontainer.o"
-
-    CZZFLAGS="-DENABLE_PYTHON_VM -DENABLE_PYTHON3 $($PYTHON3_CONFIG --includes) -I. -Wall -Werror -fPIC -pthread -std=c++14 -g"
-    g++ -shared $CZZFLAGS -I/usr/local/lib/$PYTHON3_VERSION/dist-packages/numpy/core/include $($PYTHON3_CONFIG --libs) -opyextdataframe.so python_ext_dataframe.cc || die "Failed to compile pyextdataframe.so"
+    g++ -shared $CXXFLAGS -I/usr/local/lib/$PYTHON3_VERSION/dist-packages/numpy/core/include $($PYTHON3_CONFIG --libs) -opyextdataframe.so python_ext_dataframe.cc || die "Failed to compile pyextdataframe.so"
 
     CONTAINER_CLIENT_OBJECT_FILES="exascript_python.o pythoncontainer.o $CONTAINER_CLIENT_OBJECT_FILES"
 fi
