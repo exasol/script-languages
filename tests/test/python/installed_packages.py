255fee74c5d8fe74e127989167f3b412fe5ca452
Thomas Decker <c9mdPIyDHv@quantentunnel.de>
2018-11-23 09:21:51 +0000

    added python requests library to flavors standard_60 and standard_61

---
+++ tests/test/python/installed_packages.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+# encoding: utf8
+
+import os
+import sys
+import urllib
+
+sys.path.append(os.path.realpath(__file__ + '/../../../lib'))
+import udf
+from exatest.servers import HTTPServer, MessageBox
+from exatest.utils import tempdir
+
+class PythonPackages(udf.TestCase):
+    def setUp(self):
+        self.query('DROP SCHEMA SCHEMA_PACKAGES CASCADE', ignore_errors=True)
+        self.query('CREATE SCHEMA SCHEMA_PACKAGES')
+        self.query(udf.fixindent('''
+            CREATE OR REPLACE PYTHON SET SCRIPT "TESTREQ" (url varchar(1000))
+            EMITS (the_get INT) AS
+
+            import sys
+            import glob
+
+            sys.path.extend(glob.glob('/buckets/bfsdefault/default/*'))
+
+            import requests
+            def run(ctx):
+                r = requests.get('http://'+ctx.url)
+                ctx.emit(r.status_code)
+            '''))
+
+    def test_package_requests(self):
+        with tempdir() as tmp:
+            with open(os.path.join(tmp, 'foo.xml'), 'w') as f:
+                f.write('''<foo/>\n''')
+            with HTTPServer(tmp) as hs:
+                querytext = 'select testreq(\'' + hs.address[0]+ ':' + format(hs.address[1])+ '\')'
+                rows2 = self.query(querytext)
+                self.assertEqual(200, rows2[0][0])
+
+if __name__ == '__main__':
+    udf.main()
+
