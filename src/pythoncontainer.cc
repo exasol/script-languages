50c6dca781fac12dadd4eac9a96d7b810c9428c1
stmandl <stefanmandl@web.de>
2019-01-15 15:01:35 +0000

    consolidation of libraries in fancyr flavors

--- src/pythoncontainer.cc
+++ src/pythoncontainer.cc
@@ -143,26 +143,27 @@ PythonVMImpl::PythonVMImpl(bool checkOnly): m_checkOnly(checkOnly)
 {
     script_code = string("\xEF\xBB\xBF") + string(SWIGVM_params->script_code);    // Magic-Number of UTF-8 files
 
-//    const string nositeKeyword = "%nosite";
-//    const string whitespace = " \t\f\v";
-//    const string lineEnd = ";";
-//    size_t pos;
-//    string nosite = ExecutionGraph::extractOptionLine(script_code, nositeKeyword, whitespace, lineEnd, pos, [&](const char* msg){throw PythonVM::exception(msg);});
+    const string nositeKeyword = "%nosite";
+    const string whitespace = " \t\f\v";
+    const string lineEnd = ";";
+    size_t pos;
+    string nosite = ExecutionGraph::extractOptionLine(script_code, nositeKeyword, whitespace, lineEnd, pos, [&](const char* msg){throw PythonVM::exception(msg);});
 
 //    cerr << "VALUE of nosite: |" << nosite << "|" << endl;
 
 
     int noSiteFlag = 0;
-//    if (nosite == "yes") {noSiteFlag=1;}
-//    else if (nosite == "" || nosite == "no") {noSiteFlag=0;}
-//    else throw PythonVM::exception("Invalid value for %nosite option, must be yes or no");
+    if (nosite == "yes") {noSiteFlag=1;}
+    else if (nosite == "" || nosite == "no") {noSiteFlag=0;}
+    else throw PythonVM::exception("Invalid value for %nosite option, must be yes or no");
 
 //    cerr << "Value of noSiteFlag: |" << noSiteFlag << "|" << endl;
 
-//    script_code = string("\xEF\xBB\xBF") + script_code;
+    script_code = string("\xEF\xBB\xBF") + script_code;
 
 //    cerr << "Script code after extract option line: " << endl << script_code << endl;
 
+    SWIGVM_params->script_code = strdup(script_code.c_str());
 
     script = exatable = globals = retvalue = NULL;
 #ifndef DISABLE_PYTHON_SUBINTERP
