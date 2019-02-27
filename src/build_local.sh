4f3ad280d07ef32960423d90db9731f24349436f
Torsten Kilias <torsten.kilias@exasol.com>
2019-02-27 15:58:42 +0100

    disable optimized builds for exascript_java,  fix bazel warnings for header files and improve visualization

--- src/build_local.sh
+++ src/build_local.sh
@@ -1,3 +1,2 @@
 source .env
-export VERBOSE_BUILD="--subcommands --verbose_failures"
 bash build.sh $*
--- src/visualize_deps.sh
+++ src/visualize_deps.sh
@@ -1,10 +1,4 @@
-export JAVA_PREFIX=/usr/lib/jvm/java-11-openjdk-amd64
-export PYTHON2_PREFIX=/usr 
-export PYTHON2_VERSION=python2
-export PYTHON3_PREFIX=/usr 
-export PYTHON3_VERSION=python3
-export CUSTOM_PROTOBUF_BIN=/usr/local/bin/protoc 
-export CUSTOM_PROTOBUF_PREFIX=/usr/lib/x86_64-linux-gnu
+source .env
 DEPS=""
 for I in $1
 do
@@ -17,5 +11,8 @@ do
 done
 DEPS="($DEPS)"
 shift 1
-bazel query $* --noimplicit_deps "$DEPS except (filter('@bazel_tools', $DEPS) union filter('@local_config',$DEPS))" --output graph > graph.in
-dot -Tpng < graph.in > graph.png
+bazel query $* --noimplicit_deps "$DEPS except (filter('@bazel_tools', $DEPS) union filter('@local_config',$DEPS))" --output graph | sed -e "s/label/xlabel/g" > graph.in
+
+dot -Grank=max   -Gsplines=ortho  -Goverlap=false -Granksep=4 -Gnodesep=2  -Tpng < graph.in > graph.png
+
+# -Gconcentrate=true -Gsplines=ortho  -Nshape=box  -Gconcentrate=true
