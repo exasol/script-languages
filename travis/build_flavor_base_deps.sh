badbfde5c9cd02cf82414e083217e10fbc83fc1f
Torsten Kilias <torsten.kilias@exasol.com>
2019-06-07 17:57:41 +0200

    fix build_name.sh calls in travis scripts

--- travis/build_flavor_base_deps.sh
+++ travis/build_flavor_base_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-command="./exaslct push $($SCRIPT_DIR/docker_options_build.sh) --build-name "$($SCRIPT_DIR/build_name.sh)" $FORCE_REBUILD --goal flavor_base_deps --force-rebuild-from flavor_base_deps $*"
+command="./exaslct push $($SCRIPT_DIR/docker_options_build.sh) --build-name '$($SCRIPT_DIR/build_name.sh)' $FORCE_REBUILD --goal flavor_base_deps --force-rebuild-from flavor_base_deps $*"
 echo "Executing command: $command"
 bash -c "$command"
