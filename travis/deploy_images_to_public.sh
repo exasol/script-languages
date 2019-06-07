46a1988cff9e7aaf3e32c6a9c23826a2e266dea3
Torsten Kilias <torsten.kilias@exasol.com>
2019-06-07 02:54:05 +0200

    travis: add push-force for image deployment to registry

--- travis/deploy_images_to_public.sh
+++ travis/deploy_images_to_public.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-command="./exaslct push --push-all $($SCRIPT_DIR/docker_options_deploy_to_public.sh) $*"
+command="./exaslct push --force-push --push-all $($SCRIPT_DIR/docker_options_deploy_to_public.sh) $*"
 echo "Executing command: $command"
 bash -c "$command"
