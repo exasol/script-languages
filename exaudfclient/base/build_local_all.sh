c05b1a289e46e787516e830f8e775ba70e0a35f1
Torsten Kilias <tkilias@users.noreply.github.com>
2019-10-14 10:08:07 +0200

    Add alwayslink to cc_library and pin bazel to version 1.0.0 (#86)
    Fix https://github.com/exasol/script-languages/issues/85 by
    * Pinning bazel to version 1.0.0
    * Adding alwayslink to all cc_library rules

--- exaudfclient/base/build_local_all.sh
+++ exaudfclient/base/build_local_all.sh
@@ -1,1 +1,1 @@
-bash build_local.sh $* -config no-tty --config python --config r --config java --config slow-wrappers-and-fast-binaries
+bash build_local.sh $* --config no-tty --config python --config java
