7ea20b35131502a26bf13b02af5ec6293019cb6d
Torsten Kilias <torsten.kilias@exasol.com>
2019-11-15 07:58:20 +0100

    Fix path of exported container in create_github_release.sh

--- google-cloud-build/ci-scripts/scripts/create_github_release.sh
+++ google-cloud-build/ci-scripts/scripts/create_github_release.sh
@@ -9,7 +9,7 @@ then
   TAG_NAME="ci/${COMMIT:0:10}"
 fi
 find .build_output/exports -type f -size +1900M | xargs rm
-EXPORTED_CONTAINERS=.build_output/exports/*.tar.gz
+EXPORTED_CONTAINERS=.build_output/cache/exports/*.tar.gz
 GITHUB_USER="$3"
 GITHUB_TOKEN="$(cat secrets/GITHUB_TOKEN)"
 GITHUB_REPOSITORY="$4"
