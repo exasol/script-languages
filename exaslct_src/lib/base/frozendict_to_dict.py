27d807a98af2a9cb5a15fc4d93641a7b173842e3
Torsten Kilias <torsten.kilias@exasol.com>
2020-01-06 12:35:04 +0100

    Fix moved lugigi frozen dict and pin version in pipfile

--- exaslct_src/lib/base/frozendict_to_dict.py
+++ exaslct_src/lib/base/frozendict_to_dict.py
@@ -1,9 +1,8 @@
-import luigi
-
+from collections import Mapping
 
 class FrozenDictToDict():
     def convert(self, obj):
-        if isinstance(obj, luigi.parameter._FrozenOrderedDict):
+        if isinstance(obj, Mapping):
             return {k: self.convert(v) for k, v in obj.items()}
         elif isinstance(obj, (list,tuple)):
             return [self.convert(o) for o in obj]
