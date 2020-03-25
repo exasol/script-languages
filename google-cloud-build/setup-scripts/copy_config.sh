dcbca9228c85a1234d70f40553de2c9b5822738e
Torsten Kilias <torsten.kilias@exasol.com>
2020-03-25 10:30:53 +0100

    Cleanup google cloud triggers

--- google-cloud-build/setup-scripts/copy_config.sh
+++ google-cloud-build/setup-scripts/copy_config.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 env_file=".env/env.yaml"
 CONFIG_BUCKET=$(cat "$env_file" | yq -r .config_bucket)
-gsutil -m cp -r "file://.env" "$CONFIG_BUCKET"
+gsutil -m rsync $* -r "file://.env" "$CONFIG_BUCKET"
