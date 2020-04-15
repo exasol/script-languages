b92e5ef0036db77d2281cbc8cdd7d40530a5ea70
Torsten Kilias <torsten.kilias@exasol.com>
2020-04-15 10:36:33 +0200

    fix paths in google-cloud-build/download_config.sh

--- google-cloud-build/download_config.sh
+++ google-cloud-build/download_config.sh
@@ -7,6 +7,6 @@ env_file=".env/env.yaml"
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 PROJECT=$1
 CONFIG_BUCKET_NAME=$2
-CONFIG_BUCKET="gs://$CONFIG_BUCKET_NAME/.env"
+CONFIG_BUCKET="gs://$CONFIG_BUCKET_NAME/*"
 gcloud config set project "$PROJECT"
-gsutil -m cp -r "$CONFIG_BUCKET" "file://." 
+gsutil -m cp -r "$CONFIG_BUCKET" "file://.env" 
