15a491919956d2eb914c7f49932f43fd7299141e
Torsten Kilias <tkilias@users.noreply.github.com>
2020-05-06 11:38:12 +0200

    Fix githook install scripts (#137)

--- githooks/install_recursive.sh
+++ githooks/install_recursive.sh
@@ -5,22 +5,28 @@ set -o pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 REPO_DIR=$(git rev-parse --show-toplevel)
+REPO_DIR="$(readlink -f "${REPO_DIR}")"
 
+pushd $REPO_DIR &> /dev/null
 echo
 echo "Install hook for $REPO_DIR"
 echo "=================================================================="
 echo
 bash "$REPO_DIR/githooks/install.sh"
 echo
+popd
 
 submodule_paths=$(git submodule status --recursive | sed "s/^\s*//" | cut -f 2 -d " ")
 for submodule_path in $submodule_paths
 do
+    submodule_path="$REPO_DIR/$submodule_path"
+    submodule_path="$(readlink -f "${submodule_path}")"
+    pushd "$submodule_path" &> /dev/null
     echo
-    echo "Install hook for submodule $REPO_DIR/$submodule_path"
+    echo "Install hook for submodule $submodule_path"
     echo "==============================================================="
     echo
-    install_script_for_submodule="$REPO_DIR/$submodule_path/githooks/install.sh"
+    install_script_for_submodule="githooks/install.sh"
     if [[ -f "$install_script_for_submodule" ]]
     then
         bash "$install_script_for_submodule"
