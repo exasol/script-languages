9eb27cf31f4a2315cde4e403aa220d0f0d0f2121
Torsten Kilias <torsten.kilias@exasol.com>
2020-09-10 12:41:29 +0200

    Add check_if_submodules_head_are_ancestors_of_origin_master.sh to ci

---
+++ scripts/check_if_submodules_head_are_ancestors_of_origin_master.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+REPO_DIR=$(git rev-parse --show-toplevel)
+REPO_DIR="$(readlink -f "${REPO_DIR}")"
+
+EXIT_CODE=0
+submodule_paths=$(git submodule status --recursive | sed "s/^\s*//" | cut -f 2 -d " ")
+for relative_submodule_path in $submodule_paths
+do
+    submodule_path="$REPO_DIR/$relative_submodule_path"
+    submodule_path="$(readlink -f "${submodule_path}")"
+    pushd "$submodule_path" &> /dev/null
+    git fetch
+    echo
+    echo "Checking submodule: \"$relative_submodule_path\""
+      #HASH_ORIGIN_MASTER=$(git log --decorate=full --format="%H" refs/remotes/origin/master | sed "s/, /,/g")
+    HEAD_IS_ANCESTOR_OF_ORIGIN_MASTER=$(git merge-base --is-ancestor HEAD refs/remotes/origin/master || echo "$?")
+    HASH_ORIGIN_MASTER=$(git log --decorate=full --format="%H" -n 1 refs/remotes/origin/master)
+    HASH_HEAD=$(git log --decorate=full --format="%H" -n 1 HEAD)
+    if [[ "${HEAD_IS_ANCESTOR_OF_ORIGIN_MASTER}" == "" ]]
+    then
+      echo "HEAD is ancestor of origin/master"
+      echo "Hash HEAD:          $HASH_HEAD"
+      echo "Hash origin/master: $HASH_ORIGIN_MASTER"
+    else
+      echo "HEAD is not ancestor of origin/master"
+      echo "Hash HEAD:          $HASH_HEAD"
+      echo "Hash origin/master: $HASH_ORIGIN_MASTER"
+      echo "Log Line HEAD:"
+      echo "$(git log --oneline -n 1 HEAD)"
+      echo "Log Line origin/master:"
+      echo "$(git log --oneline -n 1 refs/remotes/origin/master)"
+      EXIT_CODE=1
+    fi
+    popd &> /dev/null
+done
+exit $EXIT_CODE
