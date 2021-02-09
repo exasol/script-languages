ad96a91f1f7036fd93fafac0708650654942ad51
Torsten Kilias <torsten.kilias@exasol.com>
2021-02-09 11:11:36 +0100

    Fix comparision in find_images_by_tag in docker_utils.py

--- exaslct_src/exaslct/lib/utils/docker_utils.py
+++ exaslct_src/exaslct/lib/utils/docker_utils.py
@@ -4,6 +4,6 @@ from typing import Callable, List
 def find_images_by_tag(client, condition: Callable[[str], bool]) -> List:
     images = client.images.list()
     filter_images = [image for image in images
-                     if len(image.tags) >= 1 is not None and
+                     if image.tags is not None and len(image.tags) > 0 and
                      any([condition(tag) for tag in image.tags])]
     return filter_images
