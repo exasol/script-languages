64b881de68f372073b77284edd8547bdefdf206d
Torsten Kilias <tkilias@users.noreply.github.com>
2021-03-17 10:45:47 +0100

    Fix url for get-pip.py in install_python2.7_pip.sh (#175)
    The download url for get-pip.py changed, because of refactorings of the pip maintainers.

--- ext/scripts/install_scripts/install_python2.7_pip.sh
+++ ext/scripts/install_scripts/install_python2.7_pip.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl -o get-pip.py https://bootstrap.pypa.io/2.7/get-pip.py
+curl -o get-pip.py https://bootstrap.pypa.io/pip/2.7/get-pip.py
 python2.7 get-pip.py "pip < 21"
 rm get-pip.py
