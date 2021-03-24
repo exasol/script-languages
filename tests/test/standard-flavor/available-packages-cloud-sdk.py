03ef82a4a8acbb9076134f9b3a1b49839cafde4b
Torsten Kilias <torsten.kilias@exasol.com>
2021-03-24 17:36:48 +0100

    Fix available-packages-cloud-sdk.py

--- tests/test/standard-flavor/available-packages-cloud-sdk.py
+++ tests/test/standard-flavor/available-packages-cloud-sdk.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append(os.path.realpath(__file__ + '/../../../lib'))
 
 import udf
+from exatest.testcase import useData
 
 class AvailablePythonPackages(udf.TestCase):
     def setUp(self): 
@@ -16,22 +17,22 @@ class AvailablePythonPackages(udf.TestCase):
     data = [
             ("azure.batch",), 
             ("azure.cosmos",), 
-            ("azure.eventgrid",)
-            ("azure.eventhub",)
-            ("azure.eventhub.checkpointstoreblob",)
-            ("azure.eventhub.checkpointstoreblob.aio",)
-            ("azure.identity",)
-            ("azure.keyvault",)
-            ("azure.keyvault.certificates",)
-            ("azure.keyvault.keys",)
-            ("azure.keyvault.secrets",)
-            ("azure.kusto-data",)
-            ("azure.loganalytics",)
-            ("azure.servicebus",)
-            ("azure.storage.blob",)
-            ("azure.storage.file.datalake",)
-            ("azure.storage.file.share",)
-            ("azure.storage.queue",)
+            ("azure.eventgrid",),
+            ("azure.eventhub",),
+            ("azure.eventhub.checkpointstoreblob",),
+            ("azure.eventhub.checkpointstoreblob.aio",),
+            ("azure.identity",),
+            ("azure.keyvault",),
+            ("azure.keyvault.certificates",),
+            ("azure.keyvault.keys",),
+            ("azure.keyvault.secrets",),
+            ("azure.kusto-data",),
+            ("azure.loganalytics",),
+            ("azure.servicebus",),
+            ("azure.storage.blob",),
+            ("azure.storage.file.datalake",),
+            ("azure.storage.file.share",),
+            ("azure.storage.queue",),
         ]
 
     @useData(data)
