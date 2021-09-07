48f1f70f1129b9964950b5bf2c44983d312c854a
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-09-07 14:39:02 -0300

    Made constructors of swig classes inline again.

--- exaudfclient/base/exaudflib/impl/swig/swig_result_handler.cc
+++ exaudfclient/base/exaudflib/impl/swig/swig_result_handler.cc
@@ -1,10 +1,2 @@
 #include "exaudflib/impl/swig/swig_result_handler.h"
 
-SWIGVMContainers::SWIGResultHandler_Impl::SWIGResultHandler_Impl(SWIGTableIterator* table_iterator)
-: m_table_iterator(table_iterator),
-m_connection_id(exaudflib::global.SWIGVM_params_ref->connection_id),
-m_socket(*(exaudflib::global.SWIGVM_params_ref->sock)),
-m_message_size(0),
-m_rows_emited(1),
-m_types(*(exaudflib::global.SWIGVM_params_ref->out_types))
-{}
