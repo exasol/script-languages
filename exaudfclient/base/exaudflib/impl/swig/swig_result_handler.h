5c7a5e956248163e029162f9d5efd4e0a1985177
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-09-07 14:39:02 -0300

    Made constructors of swig classes inline again.

--- exaudfclient/base/exaudflib/impl/swig/swig_result_handler.h
+++ exaudfclient/base/exaudflib/impl/swig/swig_result_handler.h
@@ -31,7 +31,15 @@ private:
     const std::vector<SWIGVM_columntype_t> &m_types;
 public:
     const char* checkException() {return SWIGGeneralIterator::checkException();}
-    SWIGResultHandler_Impl(SWIGTableIterator*);
+    SWIGResultHandler_Impl(SWIGTableIterator* table_iterator)
+    : m_table_iterator(table_iterator),
+    m_connection_id(exaudflib::global.SWIGVM_params_ref->connection_id),
+    m_socket(*(exaudflib::global.SWIGVM_params_ref->sock)),
+    m_message_size(0),
+    m_rows_emited(1),
+    m_types(*(exaudflib::global.SWIGVM_params_ref->out_types))
+    {}
+
     ~SWIGResultHandler_Impl() {}
     inline void reinitialize() {
         m_rows_emited = 0;
