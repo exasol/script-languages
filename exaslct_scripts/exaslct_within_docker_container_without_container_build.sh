3a84d84415cbce5861c8f2bdfd54284be17c72c9
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-09-16 07:09:44 -0300

    #354 Updated to exaslct v0.3.0 (#219)

--- exaslct_scripts/exaslct_within_docker_container_without_container_build.sh
+++ exaslct_scripts/exaslct_within_docker_container_without_container_build.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+#####################################################################################
+###REMEMBER TO TEST ANY CHANGES HERE ON MACOSX!!!
+#####################################################################################
+
+
 set -euo pipefail
 
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+rl=readlink
+if [[ "$(uname)" = Darwin ]]; then
+  rl=greadlink
+fi
+
+if [[ ! "$(command -v $rl)" ]]; then
+  echo readlink not available! Please install coreutils: On Linux \"apt-get install coreutils\" or similar. On MacOsX \"brew install coreutils\".
+  exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$($rl -f "${BASH_SOURCE[0]}")")"
+
 
 RUNNER_IMAGE_NAME="$1"
 shift 1
@@ -12,4 +28,9 @@ if [ -z "$FIND_IMAGE_LOCALLY" ]; then
   docker pull "$RUNNER_IMAGE_NAME"
 fi
 
-bash "$SCRIPT_DIR/exaslct_within_docker_container.sh" "$RUNNER_IMAGE_NAME" "${@}"
+EXEC_SCRIPT=exaslct_within_docker_container.sh
+if [[ "$(uname)" = Darwin ]]; then
+  EXEC_SCRIPT=exaslct_within_docker_container_slim.sh
+fi
+
+bash "$SCRIPT_DIR/$EXEC_SCRIPT" "$RUNNER_IMAGE_NAME" "${@}"
