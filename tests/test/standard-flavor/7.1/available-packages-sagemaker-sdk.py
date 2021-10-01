e22473078ceb587658f3a473a7970ab5a7ec80d5
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2021-10-01 15:50:05 +0200

    Fixes #366 Added Sagemaker into Standard flavours 7.1.0

---
+++ tests/test/standard-flavor/7.1/available-packages-sagemaker-sdk.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2.7
+
+import os
+import sys
+
+sys.path.append(os.path.realpath(__file__ + '/../../../../lib'))
+
+import udf
+from exatest.testcase import useData
+
+class AvailablePythonPackages(udf.TestCase):
+    def setUp(self): 
+        self.query('create schema available_packages', ignore_errors=True) 
+
+
+
+    data = [
+            ("sagemaker",),
+        ]
+
+    @useData(data)
+    def test_package_import(self, pkg, fail=False, alternative=None):
+        sql=udf.fixindent('''
+            CREATE OR REPLACE PYTHON3 SCALAR SCRIPT available_packages.test_import_of_package() returns int AS
+            
+            import %s
+            def run(ctx): return 1
+            /
+            ''' % (pkg))
+        print(sql)
+        self.query(sql)
+        try:
+            rows = self.query('''SELECT available_packages.test_import_of_package() FROM dual''')
+            if not fail:
+                self.assertRowsEqual([(1,)], rows)
+            else:
+                assert 'Expected Failure' == 'not found'
+        except:
+            if fail:
+                return
+            if alternative:
+                self.import_test(alternative,fail)
+            else:
+                raise
+
+
+
+
+
+if __name__ == '__main__':
+    udf.main()
+
