cb7528b456323be192d62b21f939557b0f3c6ee7
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-27 13:35:51 -0300

    Just testing....

--- exaslct_scripts/exaslct.sh
+++ exaslct_scripts/exaslct.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-EXASLCT_GIT_REF="9e08f15cd0a6ecdf27ff012817ec52272b10a7c0"
+EXASLCT_GIT_REF="21feebccfd80515010d5d6e67cba2094be682278"
 RUNNER_IMAGE_NAME="$(bash "$SCRIPT_DIR/construct_docker_runner_image_name.sh" "$EXASLCT_GIT_REF")"
 
 bash $SCRIPT_DIR/exaslct_within_docker_container_without_container_build.sh "$RUNNER_IMAGE_NAME" "${@}"
