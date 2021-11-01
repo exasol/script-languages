a81ebd63c2556c178a1b0f87ea219bcb87acc41e
Torsten Kilias <tkilias@users.noreply.github.com>
2021-11-01 18:23:01 +0100

    #331: Add python 3.8 minimal flavor (#232)
    - Add flavor python-3.8-minimal
    - Fix header and lib discovery for python in udf-client to be compatible with python 3.8
    - Refactor scripts list_candidate_versions_for_apt.sh and list_newest_versions_for_apt.sh

--- ext/scripts/list_newest_versions/list_candidate_versions_for_apt.sh
+++ ext/scripts/list_newest_versions/list_candidate_versions_for_apt.sh
@@ -7,20 +7,26 @@ set -o pipefail
 PACKAGE_LIST_FILE=$1
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-PACKAGE_LIST=$($SCRIPT_DIR/extract_columns_from_package_lisl.pl --file $PACKAGE_LIST_FILE --columns 0)
-apt-cache policy $PACKAGE_LIST \
-  | grep -A 2 -E "^[^ ]+:" \
-  | sed "s/^--/|/g" \
-  | tr "\n" " " \
-  | tr "|" "\n" \
-  | sed "s/Candidate//g" \
-  | sed "s/Installed//g" \
-  | sed "s/: / /g" \
-  | sed "s/^ //g" \
-  | sed "s/ $//g" \
-  | sed -E "s/ +/ /g" \
-  | tr " " "|" \
-  | tr "\n" " " \
-  | sed "s/^/Package|Installed|Candidate /g" \
-  | sed "s/$/ /g" \
-  | tr " " "\n" 
+PACKAGE_LIST=( $($SCRIPT_DIR/extract_columns_from_package_lisl.pl --file $PACKAGE_LIST_FILE --columns 0) )
+VERSION_LIST=( $($SCRIPT_DIR/extract_columns_from_package_lisl.pl --file $PACKAGE_LIST_FILE --columns 1) )
+echo "Package|Requested Version|Candidate Version"
+while (( ${#PACKAGE_LIST[@]} ))
+do
+  package="${PACKAGE_LIST[0]}"
+  version="${VERSION_LIST[0]}"
+  if [ "$version" == "<<<<1>>>>" ]
+  then
+    version="No version specified"
+  fi
+  set +e
+  candidate=$( apt-cache policy "$package" | grep "Candidate" | sed -E "s/^ +//g" |  sed -E "s/ +/ /g"  | cut -f2 -d " ")
+  set -e
+  if [ -z "$candidate" ]
+  then
+    candidate="Package not available"
+  fi
+
+  echo "$package|$version|$candidate"
+  PACKAGE_LIST=( "${PACKAGE_LIST[@]:1}" )
+  VERSION_LIST=( "${VERSION_LIST[@]:1}" )
+done
--- ext/scripts/list_newest_versions/list_newest_versions_for_apt.sh
+++ ext/scripts/list_newest_versions/list_newest_versions_for_apt.sh
@@ -7,12 +7,7 @@ set -o pipefail
 PACKAGE_LIST_FILE=$1
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
+echo "Package|Requested Version|Candidate Version"
 $SCRIPT_DIR/list_candidate_versions_for_apt.sh $PACKAGE_LIST_FILE \
   | sed 1d \
-  | tr "|" " " \
-  | awk '$2!=$3 {print $1 " " $2 " " $3}' \
-  | tr " " "|" \
-  | tr "\n" " " \
-  | sed "s/^/Package|Installed|Candidate /g" \
-  | tr " " "\n" 
+  | awk 'BEGIN { FS = "|" } ; $2!=$3 {print $1 "|" $2 "|" $3}'
