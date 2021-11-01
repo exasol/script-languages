d0a852a2f81809de2566eac1d5ae836596106037
Torsten Kilias <torsten.kilias@exasol.com>
2021-11-01 14:32:37 +0100

    Refactor scripts list_candidate_versions_for_apt.sh and list_newest_versions_for_apt.sh

--- ext/scripts/list_newest_versions/list_newest_versions_for_apt.sh
+++ ext/scripts/list_newest_versions/list_newest_versions_for_apt.sh
@@ -10,9 +10,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 $SCRIPT_DIR/list_candidate_versions_for_apt.sh $PACKAGE_LIST_FILE \
   | sed 1d \
-  | tr "|" " " \
-  | awk '$2!=$3 {print $1 " " $2 " " $3}' \
-  | tr " " "|" \
-  | tr "\n" " " \
-  | sed "s/^/Package|Installed|Candidate /g" \
-  | tr " " "\n" 
+  | awk 'BEGIN { FS = "|" } ; $2!=$3 {print $1 "|" $2 "|" $3}' \
+  | sed "s/^/Package|Requested Version|Candidate Version\n/g" \
