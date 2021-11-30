8ba2052d3722fd72fbdb4f77b0166e0db62b9b9f
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-11-30 14:48:46 -0300

    #343: Change bazel python scripts to Python3 (#240)
    Co-authors: @tkilias , @MarleneKress79789

--- exaudfclient/base/build_integrated.py
+++ exaudfclient/base/build_integrated.py
@@ -1,7 +1,5 @@
-import os
-import tarfile
-import re
 import sys
+
 
 def build_integrated(target, source):
     assert(len(target) == 1 and len(source) > 0)
@@ -15,23 +13,23 @@ def build_integrated(target, source):
             fname_short = fname_short[len(builddir):]
         fvar = 'integrated_' + fname_short.lower().replace('.', '_').replace('/', '_')
         flines = []
-        for line in open(str(fname)):
-            line = line.replace(", PACKAGE='exascript_r'", '')
-            line = line.replace('\\', '\\\\')
-            line = line.replace(r'"', r'\"')
-            line = line.replace('\r', r'\r')
-            line = line.replace('\n', r'\n')
-            line = line.replace('RUNTIME_PATH', RE)
-            flines.append(line)
-        output.append('static const char *' + fvar + ' = "' + ''.join(flines) + '";\n');
-    fd = open(str(target[0]), 'w')
-    fd.write(''.join(output))
-    fd.close()
+        with open(str(fname)) as fname_file:
+            for line in fname_file:
+                line = line.replace(", PACKAGE='exascript_r'", '')
+                line = line.replace('\\', '\\\\')
+                line = line.replace(r'"', r'\"')
+                line = line.replace('\r', r'\r')
+                line = line.replace('\n', r'\n')
+                line = line.replace('RUNTIME_PATH', RE)
+                flines.append(line)
+        output.append('static const char *' + fvar + ' = "' + ''.join(flines) + '";\n')
+    with open(str(target[0]), 'w') as fd:
+        fd.write(''.join(output))
     return 0
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print 'Usage: build_integrated.py target source_1 source_2 ...'
+        print('Usage: build_integrated.py target source_1 source_2 ...')
         sys.exit(1)
     build_integrated(sys.argv[1:2], sys.argv[2:])
