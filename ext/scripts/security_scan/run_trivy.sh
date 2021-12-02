6275f77393d93dc0d525bd2028aedb8ad39ce270
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-12-02 14:01:00 -0300

    1. trivy - Show only critical/high in summary

--- ext/scripts/security_scan/run_trivy.sh
+++ ext/scripts/security_scan/run_trivy.sh
@@ -11,6 +11,6 @@ output_path=$1
 
 trivy rootfs --no-progress --format json --output "$output_path/trivy_report.json" /
 #run with format table and print to stdout
-trivy rootfs --no-progress --format table /
+trivy rootfs --no-progress --format table --output "$output_path/trivy_report.txt" /
 #Force script to return with error if a high or critical issue is found
-trivy rootfs --no-progress --severity "HIGH,CRITICAL" --exit-code 1 / > /dev/null
+trivy rootfs --no-progress --severity "HIGH,CRITICAL" --exit-code 1 /
