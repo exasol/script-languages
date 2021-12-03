5b6c16fa0b25412ab59af22842a28d9f351f9279
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-12-03 16:16:21 -0300

    Added google cloud build script

---
+++ google-cloud-build/ci-scripts/scripts/security_scan.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+REBUILD=$2
+FLAVOR=$1
+ADDITIONAL_ARGUMENTS=""
+if [ "$REBUILD" == "True" ]
+then
+  ADDITIONAL_ARGUMENTS="--force-rebuild"
+fi
+BUILD_PARAMETER="--no-shortcut-build"
+SYSTEM_PARAMETER="--workers 7"
+if [ -f /workspace/build-status.txt ]
+then
+  rm /workspace/build-status.txt
+fi
+touch /workspace/build-status.txt
+COMMAND="./exaslct security-scan --flavor-path "flavors/$FLAVOR" $BUILD_PARAMETER $ADDITIONAL_ARGUMENTS $SYSTEM_PARAMETER"
+echo "Executing Command: $COMMAND"
+$COMMAND || echo "fail" >> /workspace/build-status.txt
+echo
+
+echo "/workspace/build-status.txt"
+cat /workspace/build-status.txt
+echo
+
+SECURITY_REPORT_OUTPUT_PATH=".build_output/security_scan"
+
+echo "============= SECURITY REPORT ==========="
+echo
+cat "$SECURITY_REPORT_OUTPUT_PATH/security_report"
