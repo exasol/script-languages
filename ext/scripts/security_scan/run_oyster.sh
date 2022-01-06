ceab219bb11ddf54b3b05c3f3d041407327c36f6
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-01-06 14:05:46 -0300

    Fix run_oyster.sh

--- ext/scripts/security_scan/run_oyster.sh
+++ ext/scripts/security_scan/run_oyster.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 output_path=$1
 
 echo Start oysteR...
-if [[ $(Rscript "$SCRIPT_DIR/exec_oyster.R" "$output_path" &> "$output_path/oyster_debug.log") ]]; then
+if Rscript "$SCRIPT_DIR/exec_oyster.R" "$output_path" &> "$output_path/oyster_debug.log" ; then
   echo "No vulnerabilities found!"
 else
   cat "$output_path/oyster_debug.log"
