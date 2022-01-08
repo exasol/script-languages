c2347010685e3cbc4e3a5d135dc232ebbf1526c8
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-01-08 09:25:18 -0300

    Fixed test.

--- exaudfclient/base/javacontainer/test/cpp/javacontainer_test.cc
+++ exaudfclient/base/javacontainer/test/cpp/javacontainer_test.cc
@@ -5,12 +5,12 @@
 
 TEST(JavaContainer, basic_inline) {
     const std::string script_code = "%scriptclass com.exasol.udf_profiling.UdfProfiler;\n"
-                                    "%jar /buckets/bfsdefault/myudfs/ProfilingUdf-1.0-SNAPSHOT.jar;";
+                                    "%jar javacontainer/test/test.jar;";
     JavaVMTest vm(script_code);
     EXPECT_EQ(vm.getJavaVMInternalStatus().m_exaJavaPath, "/exaudf/javacontainer");
-    EXPECT_EQ(vm.getJavaVMInternalStatus().m_localClasspath, "");
-    EXPECT_EQ(vm.getJavaVMInternalStatus().m_scriptCode, "");
-    EXPECT_EQ(vm.getJavaVMInternalStatus().m_exaJarPath, "");
-    EXPECT_EQ(vm.getJavaVMInternalStatus().m_classpath, "");
+    EXPECT_EQ(vm.getJavaVMInternalStatus().m_localClasspath, "/tmp");
+    EXPECT_EQ(vm.getJavaVMInternalStatus().m_scriptCode, "\n");
+    EXPECT_EQ(vm.getJavaVMInternalStatus().m_exaJarPath, "/exaudf/javacontainer/libexaudf.jar");
+    EXPECT_EQ(vm.getJavaVMInternalStatus().m_classpath, "/exaudf/javacontainer/libexaudf.jar:javacontainer/test/test.jar");
 
 }
