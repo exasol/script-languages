742ca473e6ffc208a0292e3a3ca5e788f8401130
Torsten Kilias <torsten.kilias@exasol.com>
2022-01-15 22:52:09 +0100

    Fix list_candidate_versions_for_pip.sh

--- ext/scripts/list_newest_versions/list_candidate_versions_for_pip.sh
+++ ext/scripts/list_newest_versions/list_candidate_versions_for_pip.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 package_list_file=$1
-perl "$SCRIPT_DIR/extract_columns_from_package_lisl.pl" --file "$package_list_file" --columns 0,1 | xargs -n 1 -I{} python "$SCRIPT_DIR/get_versions.py" "{}"
+perl "$SCRIPT_DIR/extract_columns_from_package_lisl.pl" --file "$package_list_file" --columns 0,1 | xargs -n 1 -I{} python3 "$SCRIPT_DIR/get_versions.py" "{}"
