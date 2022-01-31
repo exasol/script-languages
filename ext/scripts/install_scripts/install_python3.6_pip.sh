0d67f7f7f885290bc3fec026bfcee6e3770b8bcb
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-01-31 08:26:24 -0300

    #474: Fix Python3.6 installation

--- ext/scripts/install_scripts/install_python3.6_pip.sh
+++ ext/scripts/install_scripts/install_python3.6_pip.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
+curl -o get-pip.py https://bootstrap.pypa.io/pip/3.6/get-pip.py
 python3.6 get-pip.py "pip < 21"
 rm get-pip.py
 rm -rf "$(python3.6 -m pip cache dir)"
