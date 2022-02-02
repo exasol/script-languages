2d7671a0819e2d3435bfb4590fab561b897e4a4f
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-02-02 14:01:24 -0300

    #484: Update exaslct to 0.9.0  (#262)

--- exaslct_scripts/exaslct.sh
+++ exaslct_scripts/exaslct.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+#####################################################################################
+###REMEMBER TO TEST ANY CHANGES HERE ON MACOSX!!!
+#####################################################################################
+
 set -euo pipefail
 
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+rl=readlink
+if [[ "$(uname)" = Darwin ]]; then
+  rl=greadlink
+fi
 
-EXASLCT_GIT_REF="0.8.0"
+if [[ ! "$(command -v $rl)" ]]; then
+  echo readlink not available! Please install coreutils: On Linux \"apt-get install coreutils\" or similar. On MacOsX \"brew install coreutils\".
+  exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$($rl -f "${BASH_SOURCE[0]}")")"
+
+EXASLCT_GIT_REF="0.9.0"
 RUNNER_IMAGE_NAME="$(bash "$SCRIPT_DIR/construct_docker_runner_image_name.sh" "$EXASLCT_GIT_REF")"
 
-bash $SCRIPT_DIR/exaslct_within_docker_container_without_container_build.sh "$RUNNER_IMAGE_NAME" "${@}"
+bash "$SCRIPT_DIR/exaslct_within_docker_container_without_container_build.sh" "$RUNNER_IMAGE_NAME" "${@}"
 
