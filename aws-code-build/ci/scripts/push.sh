8779803243bdf245cc12f08a16e19beccfd46ec9
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-03-18 15:41:00 -0300

    Test

--- aws-code-build/ci/scripts/push.sh
+++ aws-code-build/ci/scripts/push.sh
@@ -7,8 +7,6 @@ set -o pipefail
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 #shellcheck source=./google-cloud-build/ci-scripts/scripts/generate_source_target_docker_options.sh
 source "$SCRIPT_DIR/generate_source_target_docker_options.sh"
-#shellcheck source=./google-cloud-build/ci-scripts/scripts/generate_flavor_options.sh
-source "$SCRIPT_DIR/generate_flavor_options.sh"
 
 generate_flavor_options "$1"
 shift 1
@@ -17,7 +15,7 @@ generate_source_target_docker_options "$SCRIPT_DIR" "$@"
 PUSH_PARAMETER="--push-all --force-push"
 SYSTEM_PARAMETER="--workers 7"
 
-COMMAND="python3 -m exasol_script_languages_container_tool.main push $FLAVOR_OPTIONS $SOURCE_OPTIONS $TARGET_OPTIONS $PUSH_PARAMETER $SYSTEM_PARAMETER"
+COMMAND="python3 -m exasol_script_languages_container_tool.main push --flavor-path "flavors/$FLAVOR" $SOURCE_OPTIONS $TARGET_OPTIONS $PUSH_PARAMETER $SYSTEM_PARAMETER"
 echo "Executing Command: $COMMAND"
 bash -c "$COMMAND"
 echo
--- aws-code-build/run_local/run_local.sh
+++ aws-code-build/run_local/run_local.sh
@@ -8,5 +8,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 pushd "$SCRIPT_DIR/../../" &>/dev/null
 #Artifacts will be stored under /tmp
-./codebuild_build.sh -e ./aws-code-build/run_local/run_local.env -d -m -c -i aws/codebuild/ubuntu/standard:5.0 -b "$BUILD_SPEC" -p $AWS_PROFILE -a /tmp
+./codebuild_build.sh -e ./aws-code-build/run_local/run_local.env -d -m -c -i aws/codebuild/ubuntu/standard:5.0 -b "$BUILD_SPEC" -s "." -p $AWS_PROFILE -a /tmp
 popd > /dev/null
