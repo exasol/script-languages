061a2418ee512f007eb6963f1388616177d08024
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-03-21 10:03:30 -0300

    Test

--- print_secret.py
+++ print_secret.py
@@ -3,7 +3,7 @@ import sys
 def print_secret(secret :str):
     if secret == "***":
         print("Oh my god!!!")
-    print(f"Secret is: {secret}")
+    print(f"Secret is: {secret[0]}{secret[1:-1]}{secret[-1]}")
 
 
 for secret in sys.argv[1:]:
