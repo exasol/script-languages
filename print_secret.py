d5af17b6864d9aea1d802998f051165286ba155b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-03-21 09:52:52 -0300

    Test

---
+++ print_secret.py
@@ -0,0 +1,10 @@
+
+
+def print_secret(secret :str):
+    if secret == "***":
+        print("Oh my god!!!")
+    print(f"Secret is: {secret}")
+
+
+for secrets in sys.argv[1:]:
+    print_secret(secret)
