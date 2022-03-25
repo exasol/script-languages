09b21673f63bd92193d6245665979e6c8248a527
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-03-25 08:09:59 -0300

    1. Renamed GH Action check_starter_scripts.yaml 2. Shellcheck fixes in update_exaslct_starter_scripts.sh

--- githooks/update_exaslct_starter_scripts.sh
+++ githooks/update_exaslct_starter_scripts.sh
@@ -3,16 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# define colors for use in output
-no_color='\033[0m'
-grey='\033[0;90m'
-
 echo -e "Updating starter scripts."
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 ROOT_DIR="$SCRIPT_DIR/.."
-
-echo ROOT_DIR is "$ROOT_DIR"
 
 poetry run python3 -m exasol_script_languages_container_tool.main install-starter-scripts --install-path "$ROOT_DIR" --force-install
 git add "$ROOT_DIR/exaslct"
