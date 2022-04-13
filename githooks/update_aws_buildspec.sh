86e000c73973152697ac3629efb3e37208535db6
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-04-13 13:02:20 -0300

    1. added build_config.json

--- githooks/update_aws_buildspec.sh
+++ githooks/update_aws_buildspec.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 ROOT_DIR="$SCRIPT_DIR/.."
 
 pushd $ROOT_DIR > /dev/null
-poetry run python -m script_languages_container_ci_setup.main generate-buildspecs --flavor-root-path "$ROOT_DIR/flavors"
+poetry run python -m exasol_script_languages_container_ci_setup.main generate-buildspecs --flavor-root-path "$ROOT_DIR/flavors" --config-file aws-code-build/build_config.json
 
 git add "$ROOT_DIR/aws-code-build/ci"
 
