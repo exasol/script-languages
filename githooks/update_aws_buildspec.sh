7ca54646ffb803f98ed361c1704dc8e5f2c60b23
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-04-29 11:05:11 -0300

    Fix git add in update_aws_buildspec.sh

--- githooks/update_aws_buildspec.sh
+++ githooks/update_aws_buildspec.sh
@@ -11,7 +11,7 @@ ROOT_DIR="$SCRIPT_DIR/.."
 pushd $ROOT_DIR > /dev/null
 poetry run python -m exasol_script_languages_container_ci_setup.main generate-buildspecs --flavor-root-path "$ROOT_DIR/flavors" --config-file aws-code-build/build_config.json
 
-git add "$ROOT_DIR/aws-code-build/ci"
+git add "aws-code-build/ci"
 
 popd > /dev/null
 
