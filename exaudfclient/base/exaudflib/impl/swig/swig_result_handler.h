8cda67bea23cf2e6118f27d789990ed6df6afa10
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-05-18 14:11:47 -0300

    Moved zmq socket and exception handler from swig_common.h into global.h

--- exaudfclient/base/exaudflib/impl/swig/swig_result_handler.h
+++ exaudfclient/base/exaudflib/impl/swig/swig_result_handler.h
@@ -35,7 +35,7 @@ public:
     SWIGResultHandler_Impl(SWIGTableIterator* table_iterator)
     : m_table_iterator(table_iterator),
     m_connection_id(exaudflib::global.SWIGVM_params_ref->connection_id),
-    m_socket(*(exaudflib::global.SWIGVM_params_ref->sock)),
+    m_socket(*(exaudflib::global.sock)),
     m_message_size(0),
     m_rows_emited(1),
     m_types(*(exaudflib::global.SWIGVM_params_ref->out_types))
