7c24a1421ef5e9d23aa9e27641252723ead5bfd3
tkilias <torsten.kilias@exasol.com>
2022-05-30 09:37:59 +0200

    Add shebang to list_installed_conda.sh

--- ext/scripts/list_installed_scripts/list_installed_conda.sh
+++ ext/scripts/list_installed_scripts/list_installed_conda.sh
@@ -1,2 +1,7 @@
-micromamba list | tail +5 | sed -E "s/\s+/|/g" | cut -f2,3,5 -d "|" | sort -f -d
+#!/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
+micromamba list | tail +5 | sed -E "s/\s+/|/g" | cut -f2,3,5 -d "|" | sort -f -d
