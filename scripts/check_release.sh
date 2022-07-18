8177534706004f00be86ca4f1e91fc9729b27261
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-07-18 11:33:09 -0300

    Fixed check_release.sh

--- scripts/check_release.sh
+++ scripts/check_release.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-#shellcheck source=./scripts/build/setup_poetry_env.sh
+#shellcheck source=./scripts/setup_poetry_env.sh
 source "$SCRIPT_DIR/setup_poetry_env.sh" "$@"
 
 poetry run python3 -u "$SCRIPT_DIR/check_release.py"
