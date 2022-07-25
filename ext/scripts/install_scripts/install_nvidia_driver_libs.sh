ed96c2525d3c61783e5431525ec500eb62473a06
Torsten Kilias <torsten.kilias@exasol.com>
2022-07-25 11:46:08 +0200

    Fix shellcheck findings

--- ext/scripts/install_scripts/install_nvidia_driver_libs.sh
+++ ext/scripts/install_scripts/install_nvidia_driver_libs.sh
@@ -28,7 +28,7 @@ popd
 popd
 
 pushd "$LIB_DIR"
-for lib_file in *$DRIVER_VERSION; do
+for lib_file in *"$DRIVER_VERSION"; do
     lib_file_base="${lib_file%.$DRIVER_VERSION}"
     ln -sf "$lib_file" "$lib_file_base.1"
     ln -sf "$lib_file_base.1" "$lib_file_base"
