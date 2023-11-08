5383be348eae94becfd1593a7b37fe64d6c5dd1d
Torsten Kilias <torsten.kilias@exasol.com>
2023-11-08 14:05:18 +0100

    Update package managment tests to ubuntu 20.04

--- ext/scripts/install_scripts/install_python2.7_pip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-curl -o get-pip.py https://bootstrap.pypa.io/pip/2.7/get-pip.py
-python2.7 get-pip.py "pip < 21"
-rm get-pip.py
-rm -rf "$(python2.7 -m pip cache dir)"
