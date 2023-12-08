39583b9519db8733209d46220de3658f82a888be
Torsten Kilias <torsten.kilias@exasol.com>
2023-12-08 12:38:02 +0100

    Fix handling of character '~' in scripts/update_apt_package_in_package_list.sh

--- scripts/update_apt_package_in_package_list.sh
+++ scripts/update_apt_package_in_package_list.sh
@@ -11,19 +11,20 @@ PACKAGE=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 1 -d "|")
 CANDIDATE_VERSION=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 3 -d "|")
 CURRENT_VERSION=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 2 -d "|")
 
-FILES=$(grep -E -R "^$PACKAGE\|$CURRENT_VERSION" "$SEARCH_DIRECTORY" | cut -f 1 -d ":")
+FILES=$(grep -R "^$PACKAGE|$CURRENT_VERSION" "$SEARCH_DIRECTORY" | cut -f 1 -d ":")
 for FILE in $FILES
 do
   echo "Found package $PACKAGE|$CURRENT_VERSION in $FILE"
   echo "Original lines:"
-  grep -E "^$PACKAGE\|$CURRENT_VERSION" "$FILE"
+  grep "^$PACKAGE|$CURRENT_VERSION" "$FILE"
   echo "Updated lines:"
-  SEARCH_REPLACE_PATTERN="s/^($PACKAGE\|$CURRENT_VERSION).*$/$PACKAGE|$CANDIDATE_VERSION/g"
-	sed -E "$SEARCH_REPLACE_PATTERN" "$FILE" | grep -E "^$PACKAGE\|"
+  CURRENT_VERSION_ESCAPE=${CURRENT_VERSION//\~/\\~}
+  SEARCH_REPLACE_PATTERN="s/^($PACKAGE|$CURRENT_VERSION_ESCAPE).*$/$PACKAGE|$CANDIDATE_VERSION/g"
+	sed -E "$SEARCH_REPLACE_PATTERN" "$FILE" | grep "^$PACKAGE|"
   if [[ "$REPLACE" == "yes"  ]]
   then
     echo "Updating file $FILE:"
-	  sed -i -E "$SEARCH_REPLACE_PATTERN" "$FILE"
+	  sed -E -i "$SEARCH_REPLACE_PATTERN" "$FILE"
   fi
   echo
 done
