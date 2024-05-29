8242b289f8de5da92dd22be2abfbec9807a27f62
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-05-29 08:03:49 -0300

    Use a single variable for max memory usage

--- test_container/tests/test/pandas/all/emit_dtypes_memory_leak.py
+++ test_container/tests/test/pandas/all/emit_dtypes_memory_leak.py
@@ -8,6 +8,7 @@ from exasol_python_test_framework import udf
 from exasol_python_test_framework.exatest.testcase import useData
 from exasol_python_test_framework.udf.udf_debug import UdfDebugger
 from typing import List, Tuple, Union
+import humanfriendly
 
 
 class PandasDataFrameEmitDTypesMemoryLeakCheck(udf.TestCase):
@@ -21,6 +22,8 @@ class PandasDataFrameEmitDTypesMemoryLeakCheck(udf.TestCase):
 
         self.query(f'CREATE SCHEMA {self.__class__.__name__}', ignore_errors=True)
         self.query(f'OPEN SCHEMA {self.__class__.__name__}', ignore_errors=True)
+
+    max_memory_usage_bytes = humanfriendly.parse_size("200KB")
 
     int_dataframe_value_str = "1"
 
@@ -40,91 +43,91 @@ class PandasDataFrameEmitDTypesMemoryLeakCheck(udf.TestCase):
     types = [
         # Full columns without None or NaN / Int
 
-        ("uint8", "integer", int_dataframe_value_str, 200000),
-        ("uint16", "integer", int_dataframe_value_str, 200000),
-        ("uint32", "integer", int_dataframe_value_str, 200000),
-        ("uint64", "integer", int_dataframe_value_str, 200000),
-        ("int8", "integer", int_dataframe_value_str, 200000),
-        ("int16", "integer", int_dataframe_value_str, 200000),
-        ("int32", "integer", int_dataframe_value_str, 200000),
-        ("int64", "integer", int_dataframe_value_str, 200000),
-        ("object", "integer", int_dataframe_value_str, 200000),
+        ("uint8", "integer", int_dataframe_value_str),
+        ("uint16", "integer", int_dataframe_value_str),
+        ("uint32", "integer", int_dataframe_value_str),
+        ("uint64", "integer", int_dataframe_value_str),
+        ("int8", "integer", int_dataframe_value_str),
+        ("int16", "integer", int_dataframe_value_str),
+        ("int32", "integer", int_dataframe_value_str),
+        ("int64", "integer", int_dataframe_value_str),
+        ("object", "integer", int_dataframe_value_str),
 
         # Full columns without None or NaN / Float
 
-        ("float16", "double", float16_dataframe_value_str, 200000),
-        ("float32", "double", float_dataframe_value_str, 200000),
-        ("float64", "double", float_dataframe_value_str, 200000),
-        ("float", "double", float_dataframe_value_str, 200000),
-        ("double", "double", float_dataframe_value_str, 200000),
-        ("object", "double", float_dataframe_value_str, 200000),
+        ("float16", "double", float16_dataframe_value_str),
+        ("float32", "double", float_dataframe_value_str),
+        ("float64", "double", float_dataframe_value_str),
+        ("float", "double", float_dataframe_value_str),
+        ("double", "double", float_dataframe_value_str),
+        ("object", "double", float_dataframe_value_str),
 
         # Full columns without None or NaN / Int to Float
 
-        ("uint8", "double", int_dataframe_value_str, 200000),
-        ("uint16", "double", int_dataframe_value_str, 200000),
-        ("uint32", "double", int_dataframe_value_str, 200000),
-        ("uint64", "double", int_dataframe_value_str, 200000),
-        ("int8", "double", int_dataframe_value_str, 200000),
-        ("int16", "double", int_dataframe_value_str, 200000),
-        ("int32", "double", int_dataframe_value_str, 200000),
-        ("int64", "double", int_dataframe_value_str, 200000),
-        ("object", "double", int_dataframe_value_str, 200000),
+        ("uint8", "double", int_dataframe_value_str),
+        ("uint16", "double", int_dataframe_value_str),
+        ("uint32", "double", int_dataframe_value_str),
+        ("uint64", "double", int_dataframe_value_str),
+        ("int8", "double", int_dataframe_value_str),
+        ("int16", "double", int_dataframe_value_str),
+        ("int32", "double", int_dataframe_value_str),
+        ("int64", "double", int_dataframe_value_str),
+        ("object", "double", int_dataframe_value_str),
 
         # Full columns without None or NaN / Float to Int
 
-        ("float16", "integer", float16_dataframe_value_str, 200000),
-        ("float32", "integer", float_dataframe_value_str, 200000),
-        ("float64", "integer", float_dataframe_value_str, 200000),
-        ("float", "integer", float_dataframe_value_str, 200000),
-        ("double", "integer", float_dataframe_value_str, 200000),
-        ("object", "integer", float_dataframe_value_str, 200000),
+        ("float16", "integer", float16_dataframe_value_str),
+        ("float32", "integer", float_dataframe_value_str),
+        ("float64", "integer", float_dataframe_value_str),
+        ("float", "integer", float_dataframe_value_str),
+        ("double", "integer", float_dataframe_value_str),
+        ("object", "integer", float_dataframe_value_str),
 
         # Full columns without None or NaN / Int to Decimal
 
-        ("uint8", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("uint16", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("uint32", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("uint64", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("int8", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("int16", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("int32", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("int64", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
-        ("object", "DECIMAL(10,5)", int_dataframe_value_str, 200000),
+        ("uint8", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("uint16", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("uint32", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("uint64", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("int8", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("int16", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("int32", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("int64", "DECIMAL(10,5)", int_dataframe_value_str),
+        ("object", "DECIMAL(10,5)", int_dataframe_value_str),
 
         # Full columns without None or NaN / Float to Decimal
 
-        ("float16", "DECIMAL(10,5)", float16_dataframe_value_str, 200000),
-        ("float32", "DECIMAL(10,5)", float_dataframe_value_str, 200000),
-        ("float64", "DECIMAL(10,5)", float_dataframe_value_str, 200000),
-        ("float", "DECIMAL(10,5)", float_dataframe_value_str, 200000),
-        ("double", "DECIMAL(10,5)", float_dataframe_value_str, 200000),
-        ("object", "DECIMAL(10,5)", float_dataframe_value_str, 200000),
+        ("float16", "DECIMAL(10,5)", float16_dataframe_value_str),
+        ("float32", "DECIMAL(10,5)", float_dataframe_value_str),
+        ("float64", "DECIMAL(10,5)", float_dataframe_value_str),
+        ("float", "DECIMAL(10,5)", float_dataframe_value_str),
+        ("double", "DECIMAL(10,5)", float_dataframe_value_str),
+        ("object", "DECIMAL(10,5)", float_dataframe_value_str),
 
         # Full columns without None or NaN / Decimal
 
-        ("object", "DECIMAL(10,5)", decimal_dataframe_value_str, 200000),
+        ("object", "DECIMAL(10,5)", decimal_dataframe_value_str),
 
         # Full columns without None or NaN / String
 
-        ("string", "VARCHAR(2000000)", str_dataframe_value_str, 200000),
-        ("object", "VARCHAR(2000000)", str_dataframe_value_str, 200000),
+        ("string", "VARCHAR(2000000)", str_dataframe_value_str),
+        ("object", "VARCHAR(2000000)", str_dataframe_value_str),
 
         # Full columns without None or NaN / Boolean
 
-        ("bool_", "boolean", bool_dataframe_value_str, 200000),
-        ("boolean", "boolean", bool_dataframe_value_str, 200000),
-        ("object", "boolean", bool_dataframe_value_str, 200000),
+        ("bool_", "boolean", bool_dataframe_value_str),
+        ("boolean", "boolean", bool_dataframe_value_str),
+        ("object", "boolean", bool_dataframe_value_str),
 
         # Full columns without None or NaN / Date and Time
 
-        ("datetime64[ns]", "timestamp", timestamp_dataframe_value_str, 200000),
-        ("object", "timestamp", timestamp_dataframe_value_str, 200000),
-        ("object", "DATE", date_dataframe_value_str, 200000),
+        ("datetime64[ns]", "timestamp", timestamp_dataframe_value_str),
+        ("object", "timestamp", timestamp_dataframe_value_str),
+        ("object", "DATE", date_dataframe_value_str),
     ]
 
     @useData(types)
-    def test_dtype_emit(self, dtype: str, sql_type: str, dataframe_value_str: str, max_memory: int):
+    def test_dtype_emit(self, dtype: str, sql_type: str, dataframe_value_str: str):
         emit_cols = [f"o{i} {sql_type}" for i in range(25)]
         emit_cols_str = ",".join(emit_cols)
         udf_def_str = udf.fixindent(f'''
@@ -149,7 +152,7 @@ class PandasDataFrameEmitDTypesMemoryLeakCheck(udf.TestCase):
                         snapshot_end = tracemalloc.take_snapshot()
                         top_stats_begin_end = snapshot_end.compare_to(snapshot_begin, 'lineno')
                         first_item = top_stats_begin_end[0] #First item is always the largest one
-                        if first_item.size_diff > {max_memory}:
+                        if first_item.size_diff > {self.max_memory_usage_bytes}:
                             raise RuntimeError(f"scalar emit UDF uses too much memory: {{first_item}}")             
 
             /
