d92cb8a92f9a5b4d1fd21a686d9546c420708e2d
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-05-31 14:22:12 -0300

    Do not print ignored CVEs to output files

--- ext/scripts/security_scan/run_trivy.sh
+++ ext/scripts/security_scan/run_trivy.sh
@@ -9,8 +9,8 @@ fi
 
 output_path=$1
 
-trivy rootfs --no-progress --offline-scan --format json --ignore-policy /trivy.rego --show-suppressed --output "$output_path/trivy_report.json" / > /dev/null
+trivy rootfs --no-progress --offline-scan --format json --ignore-policy /trivy.rego --output "$output_path/trivy_report.json" / > /dev/null
 #run with format table and print to stdout
-trivy rootfs --no-progress --offline-scan --format table --ignore-policy /trivy.rego --show-suppressed --output "$output_path/trivy_report.txt" / > /dev/null
+trivy rootfs --no-progress --offline-scan --format table --ignore-policy /trivy.rego --output "$output_path/trivy_report.txt" / > /dev/null
 #Force script to return with error if a high or critical issue is found
 trivy rootfs --no-progress --offline-scan --ignore-policy /trivy.rego --show-suppressed --severity "HIGH,CRITICAL" --exit-code 1 /
