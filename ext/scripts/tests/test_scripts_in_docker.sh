9cbd51627d026b6bde51b794f422d10fdad4cb30
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-06-12 06:35:56 -0300

    Reactivated other tests and fixed a test file

--- ext/scripts/tests/test_scripts_in_docker.sh
+++ ext/scripts/tests/test_scripts_in_docker.sh
@@ -17,10 +17,10 @@ fi
 echo Use DOCKER_TTY_OPTION="$DOCKER_TTY_OPTION"
 
 # shellcheck disable=SC2086
-#docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_apt_tests.sh --no-dry-run
+docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_apt_tests.sh --no-dry-run
 # shellcheck disable=SC2086
 RUN_PIP_TESTS_EXECUTOR="docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts \"$IMAGE_NAME\"" PATH_TO_INSTALL_SCRIPTS="/scripts/install_scripts" bash install_scripts/run_pip_tests.sh --no-dry-run
 # shellcheck disable=SC2086
-#docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_r_remotes_tests.sh --no-dry-run
+docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_r_remotes_tests.sh --no-dry-run
 # shellcheck disable=SC2086
-#docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_ppa_tests.sh --no-dry-run
+docker run $DOCKER_TTY_OPTION -w /scripts/tests/install_scripts "$IMAGE_NAME"  bash run_ppa_tests.sh --no-dry-run
