fd1f1d1878267c898b23eefdf4fac0c7e01dc590
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-08-12 08:01:19 -0300

    Fixes from review

--- exaudfclient/build_local_all.sh
+++ exaudfclient/build_local_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash build_local.sh "$@" --lockfile_mode=off --config no-tty --config python --config java --config slow-wrapper-py3
+bash build_local.sh "$@" --config no-tty --config python --config java --config slow-wrapper-py3
