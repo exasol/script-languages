8ddafe8966d6cd1d90973f79d37e3130bdcf0f6d
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-09-25 03:05:20 -0300

    Increased timeout and skip Java DB download

--- ext/scripts/security_scan/run_trivy.sh
+++ ext/scripts/security_scan/run_trivy.sh
@@ -19,8 +19,8 @@ curl -o  "$HOME/.cache/trivy/java-db/trivy-java.db" "${TRIVY_CACHE_LOCATION}/jav
 
 output_path=$1
 
-trivy rootfs  --no-progress --offline-scan --format json --skip-db-update --ignore-policy /trivy.rego --output "$output_path/trivy_report.json" / > /dev/null
+trivy rootfs  --no-progress --offline-scan --format json --timeout 15m0s --skip-java-db-update --skip-db-update --ignore-policy /trivy.rego --output "$output_path/trivy_report.json" / > /dev/null
 #run with format table and print to stdout
-trivy rootfs --no-progress --offline-scan --format table --skip-db-update --ignore-policy /trivy.rego --output "$output_path/trivy_report.txt" / > /dev/null
+trivy rootfs --no-progress --offline-scan --format table --timeout 15m0s --skip-java-db-update --skip-db-update --ignore-policy /trivy.rego --output "$output_path/trivy_report.txt" / > /dev/null
 #Force script to return with error if a high or critical issue is found
-trivy rootfs --no-progress --offline-scan --skip-db-update --ignore-policy /trivy.rego --show-suppressed --severity "HIGH,CRITICAL" --exit-code 1 /
+trivy rootfs --no-progress --offline-scan --timeout 15m0s --skip-db-update --skip-java-db-update --ignore-policy /trivy.rego --show-suppressed --severity "HIGH,CRITICAL" --exit-code 1 /
