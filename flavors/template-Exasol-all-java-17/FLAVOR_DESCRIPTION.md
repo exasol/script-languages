e9dc3d623d43a6dcb9597067441ac58fea068f63
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-10-15 13:35:20 -0300

    #980: Moved flavor template-Exasol-all-java-17 to script-languages repo

---
+++ flavors/template-Exasol-all-java-17/FLAVOR_DESCRIPTION.md
@@ -0,0 +1,10 @@
+# Details for r-4-minimal-EXASOL-6.2.0
+
+## Packages
+
+- [UDFclient dependencies](flavor_base/udfclient_deps/packages/apt_get_packages)
+- [Language dependencies](flavor_base/language_deps/packages/apt_get_packages)
+- Flavor packages
+  - [Ubuntu packages](flavor_base/flavor_base_deps/packages/apt_get_packages)
+- Customization
+  - [Ubuntu packages](flavor_customization/packages/apt_get_packages)
---
+++ flavors/template-Exasol-all-java-17/flavor_base/build_steps.py
@@ -0,0 +1,147 @@
+from typing import Dict
+
+from exasol.slc.internal.tasks.build.docker_flavor_image_task import DockerFlavorAnalyzeImageTask
+
+
+class AnalyzeUDFClientDeps(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "udfclient_deps"
+
+    def get_additional_build_directories_mapping(self) -> Dict[str, str]:
+        return {"01_nodoc": "ext/01_nodoc","scripts": "ext/scripts"}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeLanguageDeps(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "language_deps"
+
+    def get_additional_build_directories_mapping(self) -> Dict[str, str]:
+        return {"scripts": "ext/scripts"}
+
+    def requires_tasks(self):
+        return {"udfclient_deps": AnalyzeUDFClientDeps}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeBuildDeps(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "build_deps"
+
+    def get_additional_build_directories_mapping(self) -> Dict[str, str]:
+        return {"01_nodoc": "ext/01_nodoc", "scripts": "ext/scripts"}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeBuildRun(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "build_run"
+
+    def requires_tasks(self):
+        return {"build_deps": AnalyzeBuildDeps,
+                "language_deps": AnalyzeLanguageDeps}
+
+    def get_additional_build_directories_mapping(self) -> Dict[str, str]:
+        return {"exaudfclient": "exaudfclient"}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeBaseTestDeps(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "base_test_deps"
+
+    def requires_tasks(self):
+        return {"build_deps": AnalyzeBuildDeps}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeBaseTestBuildRun(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "base_test_build_run"
+
+    def requires_tasks(self):
+        return {"base_test_deps": AnalyzeBaseTestDeps,
+                "language_deps": AnalyzeLanguageDeps}
+
+    def get_additional_build_directories_mapping(self) -> Dict[str, str]:
+        return {"exaudfclient": "exaudfclient", "emulator": "emulator"}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeFlavorBaseDeps(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "flavor_base_deps"
+
+    def requires_tasks(self):
+        return {"language_deps": AnalyzeLanguageDeps}
+
+    def get_additional_build_directories_mapping(self):
+        return {"01_nodoc": "ext/01_nodoc", "scripts": "ext/scripts"}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeFlavorCustomization(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "flavor_customization"
+
+    def requires_tasks(self):
+        return {"flavor_base_deps": AnalyzeFlavorBaseDeps}
+
+
+class AnalyzeFlavorTestBuildRun(DockerFlavorAnalyzeImageTask):
+
+    def get_build_step(self) -> str:
+        return "flavor_test_build_run"
+
+    def requires_tasks(self):
+        return {"flavor_customization": AnalyzeFlavorCustomization,
+                "base_test_build_run": AnalyzeBaseTestBuildRun}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class AnalyzeRelease(DockerFlavorAnalyzeImageTask):
+    def get_build_step(self) -> str:
+        return "release"
+
+    def requires_tasks(self):
+        return {"flavor_customization": AnalyzeFlavorCustomization,
+                "build_run": AnalyzeBuildRun,
+                "language_deps": AnalyzeLanguageDeps}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
+
+
+class SecurityScan(DockerFlavorAnalyzeImageTask):
+    def get_build_step(self) -> str:
+        return "security_scan"
+
+    def requires_tasks(self):
+        return {"release": AnalyzeRelease}
+
+    def get_path_in_flavor(self):
+        return "flavor_base"
