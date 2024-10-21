6ff5329e96345cfff823f32240f62cdd67a5247b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-10-21 16:19:21 -0300

    Fixed javavm_test.cc

--- exaudfclient/base/javacontainer/test/cpp/javavm_test.cc
+++ exaudfclient/base/javacontainer/test/cpp/javavm_test.cc
@@ -18,7 +18,7 @@ JavaVMTest::JavaVMTest(std::string scriptCode, std::unique_ptr<SwigFactoryTestIm
 }
 
 void JavaVMTest::run(std::string scriptCode, std::unique_ptr<SwigFactoryTestImpl> swigFactory) {
-    std::unique_ptr<char*> script_code = ::strdup(scriptCode.c_str());
+    std::unique_ptr<char> script_code(::strdup(scriptCode.c_str()));
     SWIGVMContainers::SWIGVM_params->script_code = script_code.get();
 #ifndef USE_CTPG_PARSER
     std::unique_ptr<SWIGVMContainers::JavaScriptOptions::ScriptOptionLinesParserLegacy> parser =
