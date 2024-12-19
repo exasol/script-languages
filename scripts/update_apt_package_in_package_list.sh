eeed56f593a7c8a3560a92e5509cf38bae46b6bb
ckunki <christoph.kuhnke@exasol.com>
2024-12-19 13:50:23 +0100

    Updated script update_apt_package_in_package_list.sh once again

--- scripts/update_apt_package_in_package_list.sh
+++ scripts/update_apt_package_in_package_list.sh
@@ -15,8 +15,8 @@ SEARCH_DIRECTORY=${2:-.}
 REPLACE=${3:-no}
 
 PACKAGE=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 1 -d "|")
-CANDIDATE_VERSION=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 3 -d "|")
 CURRENT_VERSION=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 2 -d "|")
+CANDIDATE_VERSION=$(echo "$LIST_NEWEST_VERSION_OUTPUT" | cut -f 3 -d "|")
 
 FILES=$(grep -R "^$PACKAGE|$CURRENT_VERSION" "$SEARCH_DIRECTORY" | cut -f 1 -d ":")
 for FILE in $FILES; do
