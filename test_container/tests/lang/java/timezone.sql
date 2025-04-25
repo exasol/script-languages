dd552960fcdd5e4cc0b5a1ec75915b70792664fe
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-25 15:52:20 -0300

    Install tzdata via apt

--- test_container/tests/lang/java/timezone.sql
+++ test_container/tests/lang/java/timezone.sql
@@ -1,8 +1,3 @@
---CREATE <lang>  SCALAR SCRIPT
---base_pi()
---RETURNS DOUBLE AS
-
--- pi
 
 CREATE OR REPLACE java SCALAR SCRIPT
 DEFAULT_TZ()
@@ -26,7 +21,7 @@ RETURNS VARCHAR(100) AS
 import java.time.ZoneId;
 import java.util.TimeZone;
 
-class MODIFIED_TZ{
+class MODIFY_TZ_TO_NEW_YORK{
     static String run(ExaMetadata exa, ExaIterator ctx) throws Exception {
         TimeZone timeZone = TimeZone.getDefault();
         return timeZone.getDisplayName(false, TimeZone.SHORT);
