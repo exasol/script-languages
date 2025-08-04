3e84e411d3070c513c9f067803d69b31e76484f4
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-08-04 13:12:58 +0200

    Fixed findings from review

--- ext/scripts/install_scripts/install_python3.12_pip.sh
+++ ext/scripts/install_scripts/install_python3.12_pip.sh
@@ -6,4 +6,4 @@ pip_version=$1
 curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3.12 get-pip.py "$pip_version" --break-system-packages
 rm get-pip.py
-rm -rf "$(python3.10 -m pip cache dir)"
+rm -rf "$(python3.12 -m pip cache dir)"
