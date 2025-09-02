a20e232f86eabe99d27029b26c00341b71645efb
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-09-02 06:03:54 +0000

    removed ssl_repository.bzl and a commented function

--- exaudfclient/base/javacontainer/script_options/checksum.cc
+++ exaudfclient/base/javacontainer/script_options/checksum.cc
@@ -5,23 +5,6 @@ namespace SWIGVMContainers {
 
 namespace JavaScriptOptions {
 
-/**
- * The following code is dependent on openssl for calculating md5sum
- * md5sum was used to detect if two imported scripts are same
- * As we started to use unordered_set for detecting the same,
- * the following code and openssl dependency are eliminated.
- **/
-#if 0
-inline std::vector<unsigned char> scriptToMd5(const char *script) {
-    MD5_CTX ctx;
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, script, strlen(script));
-    MD5_Final(md5, &ctx);
-    return std::vector<unsigned char>(md5, md5 + sizeof(md5));
-}
-#endif
-
 
 bool Checksum::addScript(const char *script) {
     std::string strScript = std::string(script);
