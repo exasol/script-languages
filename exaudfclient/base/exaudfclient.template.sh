532e6bc7c2163219ba9b159f8f04f45e0abe6edc
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-11-21 12:54:39 +0100

    Fixed slow wrapper

--- exaudfclient/base/exaudfclient.template.sh
+++ exaudfclient/base/exaudfclient.template.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 echo "Changing to script directory  $SCRIPT_DIR"
 cd "$SCRIPT_DIR" || return 1
-export LIBPYEXADATAFRAME_DIR="$SCRIPT_DIR/base/python/python3"
+export LIBPYEXADATAFRAME_DIR="$SCRIPT_DIR/external/exaudfclient_base+/python/python3"
 export LD_LIBRARY_PATH="/opt/conda/cuda-compat/:$LD_LIBRARY_PATH" #Temporary hack for the Cuda ML flavor(s)
