512fdb02e7d3da3ab81217da8571905bf9548a6a
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-03-10 12:00:50 -0300

    #1383: Used exaslpm to build template-Exasol-all-python-3.12-cuda-conda flavor

---
+++ ext/scripts/install_scripts/_activate_current_env.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=SC2148
+
+# This script should never be called directly, only sourced:
+
+#     source _activate_current_env.sh
+
+if [[ "${MAMBA_SKIP_ACTIVATE}" == "1" ]]; then
+  return
+fi
+
+# Initialize the current shell
+eval "$("${MAMBA_EXE}" shell hook --shell=bash)"
+
+# For robustness, try all possible activate commands.
+conda activate "${ENV_NAME}" 2>/dev/null \
+  || mamba activate "${ENV_NAME}" 2>/dev/null \
+  || micromamba activate "${ENV_NAME}"
