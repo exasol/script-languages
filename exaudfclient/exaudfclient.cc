0aad8446d8517cb1d131c17e40c27ac861de3057
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-04-20 10:18:13 -0300

    Added check in exaudfclient.cc

--- exaudfclient/exaudfclient.cc
+++ exaudfclient/exaudfclient.cc
@@ -165,6 +165,20 @@ int main(int argc, char **argv) {
     }
 #endif
 
+#ifdef UDF_PLUGIN_CLIENT
+        // udf plugins might not have arguments
+#else
+    if (! ((strcmp(argv[2], "lang=python") == 0)
+           || (strcmp(argv[2], "lang=r") == 0)
+           || (strcmp(argv[2], "lang=java") == 0)
+           || (strcmp(argv[2], "lang=streaming") == 0)
+           || (strcmp(argv[2], "lang=benchmark") == 0)) )
+    {
+        std::cerr << "F-UDF-CL-LIB-1121","Remote VM type '" << argv[2] << "' not supported.");
+        return 2;
+    }
+#endif
+
     if (::setenv("HOME", "/tmp", 1) == -1)
     {
         throw SWIGVM::exception("Failed to set HOME directory");
