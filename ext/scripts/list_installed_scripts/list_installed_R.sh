853a99cba62273f82a8db4197ea14a4cf82b60d3
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-06-18 09:53:06 -0300

    #1485: Updated exaslpm to v1.3.1

--- ext/scripts/list_installed_scripts/list_installed_R.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-set -o pipefail
-
-Rscript -e 'installed.packages()[,c("Package","Version")]' | sed "s/  */|/g" | cut -f 2,3 -d "|" | sed "s/\"//g" | sort -f -d
--- ext/scripts/list_installed_scripts/list_installed_apt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-set -o pipefail
-
-apt list | grep installed | cut -f 1,2 -d " " | sed "s#/.* #|#g" | sort -f -d 
