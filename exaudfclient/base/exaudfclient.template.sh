1ecd94c90b8791995711f96b25c0607e976f5a38
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-08-05 14:52:42 -0300

    Changed LD_LIBRARY_PATH in exaudfclient.template.sh and reverted CONDA_OVERRIDE_CUDA in packages.yml

--- exaudfclient/base/exaudfclient.template.sh
+++ exaudfclient/base/exaudfclient.template.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 echo "Changing to script directory  $SCRIPT_DIR"
 cd "$SCRIPT_DIR" || return 1
 export LIBPYEXADATAFRAME_DIR="$SCRIPT_DIR/external/exaudfclient_base+/python/python3"
-export LD_LIBRARY_PATH="/opt/conda/cuda-compat/:$LD_LIBRARY_PATH" #Temporary hack for the Cuda ML flavor(s)
+export LD_LIBRARY_PATH="/opt/conda/cuda-compat/:/opt/conda/nvvm/lib64/:$LD_LIBRARY_PATH" #Temporary hack for the Cuda ML flavor(s)
